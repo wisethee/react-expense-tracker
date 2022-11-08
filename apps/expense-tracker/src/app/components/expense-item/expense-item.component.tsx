d0ac4474484d7e7740dd9f8e5bd8f4ddef3ebf36
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-08 11:31:36 +0000

    feat: add card component, fix console warning message

--- apps/expense-tracker/src/app/components/expense-item/expense-item.component.tsx
+++ apps/expense-tracker/src/app/components/expense-item/expense-item.component.tsx
@@ -1,3 +1,4 @@
+import Card from '../card/card.component';
 import ExpenseDate from '../expense-date/expense-date.component';
 import { ExpeseItemProps } from '../types/expense-item';
 import './expense-item.component.scss';
@@ -6,13 +7,15 @@ const ExpenseItem = (props: ExpeseItemProps) => {
   const { date, title, amount } = props;
 
   return (
-    <div className="expense-item">
-      <ExpenseDate date={date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{amount}</div>
+    <Card>
+      <div className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{amount}</div>
+        </div>
       </div>
-    </div>
+    </Card>
   );
 };
 
