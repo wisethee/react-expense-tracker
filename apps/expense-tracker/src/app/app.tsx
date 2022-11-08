c6292bdc86ef1ecd88b1a6ab24cb5d84fb9e6b37
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-08 11:02:49 +0000

    feat: add expense-date and expense-item components

--- apps/expense-tracker/src/app/app.tsx
+++ apps/expense-tracker/src/app/app.tsx
@@ -1,5 +1,54 @@
+import { Fragment } from 'react';
+import ExpenseItem from './components/expense-item/expense-item.component';
+
 const App = () => {
-  return <div>App</div>;
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+    <Fragment>
+      <div>App</div>
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ apps/expense-tracker/src/app/components/expense-item/expense-item.component.tsx
@@ -0,0 +1,24 @@
+import ExpenseDate from '../expense-date/expense-date.component';
+import './expense-item.component.scss';
+
+type ExpeseItemProps = {
+  date: Date;
+  title: string;
+  amount: number;
+};
+
+const ExpenseItem = (props: ExpeseItemProps) => {
+  const { date, title, amount } = props;
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
