b70d5af77f71895f5a462f0a6b7a635e1f2122ca
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-09 12:20:21 +0000

    add expense form component

--- apps/expense-tracker/src/app/components/expense-item/expense-item.component.tsx
+++ apps/expense-tracker/src/app/components/expense-item/expense-item.component.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Card from '../card/card.component';
 import ExpenseDate from '../expense-date/expense-date.component';
 import { ExpeseItemProps } from '../types/expense-item';
@@ -6,15 +7,20 @@ import './expense-item.component.scss';
 const ExpenseItem = (props: ExpeseItemProps) => {
   const { date, title, amount } = props;
 
+  const [itemTitle, setItemTitle] = useState(title);
+
+  const clickHandler = () => {
+    setItemTitle('Updated');
+  };
+
   return (
-    <Card>
-      <div className="expense-item">
-        <ExpenseDate date={date} />
-        <div className="expense-item__description">
-          <h2>{title}</h2>
-          <div className="expense-item__price">{amount}</div>
-        </div>
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{itemTitle}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
---
+++ apps/expense-tracker/src/app/components/new-expense/new-expense.component.tsx
@@ -0,0 +1,13 @@
+import ExpenseForm from '../expense-form/expense-form.component';
+
+import './new-expense.component.scss';
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
