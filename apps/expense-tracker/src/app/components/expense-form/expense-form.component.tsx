5360d3c3d80d53c2a92d3a3140db8db201010bb9
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-09 14:41:57 +0000

    feat: manage expense form state

--- apps/expense-tracker/src/app/components/expense-form/expense-form.component.tsx
+++ apps/expense-tracker/src/app/components/expense-form/expense-form.component.tsx
@@ -1,22 +1,56 @@
+import { ChangeEvent, useState } from 'react';
 import './expense-form.component.scss';
 
+const initialState = {
+  enteredTitle: '',
+  enteredAmount: '',
+  enteredDate: '',
+};
+
 const ExpenseForm = () => {
+  // const [enteredTitle, setEnteredTitle] = useState<string>('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('');
+
+  const [userInput, setUserInput] = useState(initialState);
+
+  const formChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <form action="">
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" name="enteredTitle" onChange={formChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" />
+          <input
+            type="number"
+            name="enteredAmount"
+            onChange={formChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min={'2019-01-01'} max={'2022-12-31'} />
+          <input
+            type="date"
+            name="enteredDate"
+            min={'2019-01-01'}
+            max={'2022-12-31'}
+            onChange={formChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
