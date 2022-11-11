2d92dab2ea5fcd9736019a66735da8c271482dff
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-11 08:17:33 +0000

    feat: replace hard coded expenses components with map

--- apps/expense-tracker/src/app/components/expenses/expenses.component.tsx
+++ apps/expense-tracker/src/app/components/expenses/expenses.component.tsx
@@ -19,34 +19,16 @@ const Expenses = (props: ExpensesProps) => {
   };
 
   return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        <ExpenseItem
-          date={expenses[0].date}
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-        />
-        <ExpenseItem
-          date={expenses[1].date}
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-        />
-        <ExpenseItem
-          date={expenses[2].date}
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-        />
-        <ExpenseItem
-          date={expenses[3].date}
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-        />
-      </Card>
-    </div>
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
+      {expenses.map(({ date, title, amount }) => (
+        <ExpenseItem date={date} title={title} amount={amount} />
+      ))}
+    </Card>
   );
 };
 
