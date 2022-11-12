adfadd36be69f327605171901a7621db3b2368d3
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-11-12 18:04:01 +0000

    feat: add expenses filter by year

--- apps/expense-tracker/src/app/core/components/expenses/expenses.tsx
+++ apps/expense-tracker/src/app/core/components/expenses/expenses.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Expense } from '../../types/expense';
+import ExpensesFilter from '../expenses-filter/expenses-filter';
 import ExpensesList from '../expenses-list/expenses-list';
 
 import './expenses.scss';
@@ -10,8 +12,23 @@ type ExpensesProps = {
 const Expenses = (props: ExpensesProps) => {
   const { expenses } = props;
 
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear: string) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = expenses.filter(
+    (expense) => expense.date.getFullYear() === Number(filteredYear)
+  );
+
   return (
     <div className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
       <div className="expenses-header">
         <div className="expenses-header-start">Date</div>
         <div className="expensed-header-end">
@@ -19,7 +36,7 @@ const Expenses = (props: ExpensesProps) => {
           <div className="expenses-price">Price</div>
         </div>
       </div>
-      <ExpensesList expenses={expenses} />
+      <ExpensesList expenses={filteredExpenses} />
     </div>
   );
 };
